f4aa8d1e875d0a090fed4a43fff2bb1aa7054fcf
richmcateer <rmcateer1@gmail.com>
2020-01-27 12:54:33 -0500

    Begin bonus

--- src/index.js
+++ src/index.js
@@ -1,17 +1,3 @@
-console.log('Hello World!');
+const buildMarker = require('./marker');
 
-const mapboxgl = require("mapbox-gl");
-const buildMarker = require("./marker");
-
-
-// mapboxgl.accessToken = "pk.eyJ1IjoibGludXMxMjMiLCJhIjoiY2s1d29tNW0yMGVhYTNscWI5a3R6bHhrMCJ9.PrVmxg6g-R9CfqDS3miaWA";
-
-// const map = new mapboxgl.Map({
-//   container: "map",
-//   center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
-//   zoom: 12, // starting zoom
-//   style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
-// });
-
-
-buildMarker("hotel", -74.009, 40.705);
+buildMarker('hotel', -74.009, 40.705);
--- src/marker.js
+++ src/marker.js
@@ -1,45 +1,38 @@
-const mapboxgl = require("mapbox-gl");
+const mapboxgl = require('mapbox-gl');
 
+const activityImg = 'http://i.imgur.com/WbMOfMl.png';
+const hotel = 'http://i.imgur.com/D9574Cu.png';
+const rest = 'http://i.imgur.com/cqR6pUI.png';
 
-const activityImg = "http://i.imgur.com/WbMOfMl.png";
-const hotel = "http://i.imgur.com/D9574Cu.png";
-const rest = "http://i.imgur.com/cqR6pUI.png";
-
-mapboxgl.accessToken = "pk.eyJ1IjoibGludXMxMjMiLCJhIjoiY2s1d29tNW0yMGVhYTNscWI5a3R6bHhrMCJ9.PrVmxg6g-R9CfqDS3miaWA";
+mapboxgl.accessToken =
+  'pk.eyJ1IjoibGludXMxMjMiLCJhIjoiY2s1d29tNW0yMGVhYTNscWI5a3R6bHhrMCJ9.PrVmxg6g-R9CfqDS3miaWA';
 
 const map = new mapboxgl.Map({
-  container: "map",
+  container: 'map',
   center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
   zoom: 12, // starting zoom
-  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+  style: 'mapbox://styles/mapbox/streets-v10' // mapbox has lots of different map styles available.
 });
 
-
 function createDomEl(type) {
-  const markerDomEl = document.createElement("div"); // Create a new, detached DIV
-  markerDomEl.style.width = "32px";
-  markerDomEl.style.height = "39px";
-  if (type === "activity") {
+  const markerDomEl = document.createElement('div'); // Create a new, detached DIV
+  markerDomEl.style.width = '32px';
+  markerDomEl.style.height = '39px';
+  if (type === 'activity') {
     markerDomEl.style.backgroundImage = `url(${activityImg})`;
   }
-  if (type === "hotel") {
-    console.log('hotel called');
+  if (type === 'hotel') {
     markerDomEl.style.backgroundImage = `url(${hotel})`;
   }
-  if (type === "restaurant") {
+  if (type === 'restaurant') {
     markerDomEl.style.backgroundImage = `url(${rest})`;
   }
   return markerDomEl;
 }
 
-
 function buildMarker(type, long, lat) {
   const value = createDomEl(type);
-  console.log(value);
   return new mapboxgl.Marker(value).setLngLat([long, lat]).addTo(map);
-
-
 }
-
 
 module.exports = buildMarker;
