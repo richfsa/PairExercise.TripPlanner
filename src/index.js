d961a6261455597948ad26fbba59c3474115e3f9
Sunil Abraham <suni.abraham@gmail.com>
2020-01-27 12:27:30 -0500

    buildMarker works

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ const mapboxgl = require("mapbox-gl");
 const buildMarker = require("./marker");
 
 
-mapboxgl.accessToken = "pk.eyJ1IjoibGludXMxMjMiLCJhIjoiY2s1d29tNW0yMGVhYTNscWI5a3R6bHhrMCJ9.PrVmxg6g-R9CfqDS3miaWA";
+// mapboxgl.accessToken = "pk.eyJ1IjoibGludXMxMjMiLCJhIjoiY2s1d29tNW0yMGVhYTNscWI5a3R6bHhrMCJ9.PrVmxg6g-R9CfqDS3miaWA";
 
-const map = new mapboxgl.Map({
-  container: "map",
-  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
-  zoom: 12, // starting zoom
-  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
-});
+// const map = new mapboxgl.Map({
+//   container: "map",
+//   center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+//   zoom: 12, // starting zoom
+//   style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+// });
 
 
-new mapboxgl.Marker().setLngLat([-74.009151, 40.705086]).addTo(map);
+buildMarker("hotel", -74.009, 40.705);
